f6d2ac18edcaf28500878990cc9ce919b73d4443
ndistefano <nadege.diste@free.fr>
2021-11-05 16:22:41 +0100

    ajout fichiers

--- td1.js
+++ td1.js
@@ -25,24 +25,24 @@ function bonjour() {
 
     
     
-    function merci() {
-        
-        let saisie = document.getElementById("prenom").value;
-        let adresse = document.getElementById("mail").value;
+function merci() {
+    
+    let saisie = document.getElementById("prenom").value;
+    let adresse = document.getElementById("mail").value;
 
-        
-        { 
-        console.log(adresse);               
-        console.log(document.getElementById("message2").innerText="Nous te confirmons que tu seras informé(e) sur ton email : " + adresse);
-        document.getElementById("changerTexte2").style.display = "block";
-        document.getElementById("changerTexte").style.display = "none";
-        document.getElementById("demande").style.display = "none";
-        document.getElementById("mail").style.display = "none";
-        document.getElementById("confirmMail").style.display = "none";
-        document.getElementById("message").style.display = "none";
-        }
+    
+    { 
+    console.log(adresse);               
+    console.log(document.getElementById("message2").innerText="Nous te confirmons que tu seras informé(e) sur ton email : " + adresse);
+    document.getElementById("changerTexte2").style.display = "block";
+    document.getElementById("changerTexte").style.display = "none";
+    document.getElementById("demande").style.display = "none";
+    document.getElementById("mail").style.display = "none";
+    document.getElementById("confirmMail").style.display = "none";
+    document.getElementById("message").style.display = "none";
     }
-        document.getElementById("confirmMail").addEventListener("click", merci);
+}
+    document.getElementById("confirmMail").addEventListener("click", merci);
 
 
         
