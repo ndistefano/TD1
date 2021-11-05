1681bd7a305b3873cd930eceb54fafd6aed27214
ndistefano <nadege.diste@free.fr>
2021-11-05 13:37:37 +0100

    ajout fichiers

--- td1.js
+++ td1.js
@@ -1,27 +1,47 @@
-function bonjour() {
+   
+document.getElementById("etape4").style.display = "none"; 
+
+
+
+function bonjour() {    
     let saisie = document.getElementById("prenom").value;
+    document.getElementById("changerTexte2").style.display = "none";
 
     { 
     console.log(saisie);    
     document.getElementById("changerTexte").innerText="Bonjour "+ saisie ;
     document.getElementById("message").innerText="Le site n’est pas tout à fait terminé ! Pour être informé de son ouverture officielle, n’hésites pas à laisser ton mail.";   
     document.getElementById("demande").innerText="Votre adresse mail";
+    document.getElementById("etape4").style.display = "block"; 
+    document.getElementById("changerTexte2").innerText="Merci "+ saisie + " et à bientôt!" ; 
+    document.getElementById("accueil").style.display = "none";
+    bonjour.stopPropagation(); 
     }
 }
+
     document.getElementById("valider").addEventListener("click", bonjour); 
+
+    
     
     function merci() {
+        
         let saisie = document.getElementById("prenom").value;
         let adresse = document.getElementById("mail").value;
+        
         { 
-        console.log(adresse);        
+        console.log(adresse);               
         alert(document.getElementById("message2").innerText="Merci "+ saisie +"," + "nous te confirmons que tu seras informé sur ton email :" + adresse);
+        
         }
     }
-
         document.getElementById("confirmMail").addEventListener("click", merci);
+        
+
+    
+
+ 
   
-          
 
-
-
+      
+        
+     
