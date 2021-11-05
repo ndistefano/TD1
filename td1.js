2b1d423eb23e79c11e041884e59b9d5c7c351694
ndistefano <nadege.diste@free.fr>
2021-11-05 09:23:17 +0100

    ajout fichiers

--- td1.js
+++ td1.js
@@ -1,4 +1,27 @@
+function bonjour() {
+    let saisie = document.getElementById("prenom").value;
 
+    { 
+    console.log(saisie);    
+    document.getElementById("changerTexte").innerText="Bonjour "+ saisie ;
+    document.getElementById("message").innerText="Le site n’est pas tout à fait terminé ! Pour être informé de son ouverture officielle, n’hésites pas à laisser ton mail.";   
+    document.getElementById("demande").innerText="Votre adresse mail";
+    }
+}
+    document.getElementById("valider").addEventListener("click", bonjour); 
+    
+    function merci() {
+        let saisie = document.getElementById("prenom").value;
+        let adresse = document.getElementById("mail").value;
+        { 
+        console.log(adresse);        
+        alert(document.getElementById("message2").innerText="Merci "+ saisie +"," + "nous te confirmons que tu seras informé sur ton email :" + adresse);
+        }
+    }
+
+        document.getElementById("confirmMail").addEventListener("click", merci);
+  
+          
 
 
 
