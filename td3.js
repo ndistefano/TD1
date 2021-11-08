0c5696d6f401557a43b57d5b98e1e7c21c69ee13
ndistefano <nadege.diste@free.fr>
2021-11-08 16:52:47 +0100

    ajout fichiers td3

--- td3.js
+++ td3.js
@@ -1,9 +1,90 @@
    
 document.getElementById("etape4").style.display = "none"; 
 
-document.getElementById('etape4').addEventListener('submit',function(e){
+document.getElementById("etape4").addEventListener("submit",function(e){
     e.preventDefault();
 })
+
+
+function cookies() {        
+    { 
+    console.log();               
+    document.getElementById("cookies").style.display = "none";
+    }
+}
+    document.getElementById("acceptbutton").addEventListener("click", cookies);
+
+
+function cookies2() {    
+    { 
+    console.log();               
+    document.getElementById("popin").style.display = "flex"; 
+    document.getElementById("cookies").style.display = "none";
+        
+    }
+}    
+    document.getElementById("personalbutton").addEventListener("click", cookies2);
+
+
+function valider () {    
+
+    if (this.id == "a1") {
+        document.getElementById("a1").style.background = "green"; 
+        document.getElementById("r1").style.background = "grey"; 
+    }
+
+    else {
+        document.getElementById("a1").style.background = "grey"; 
+        document.getElementById("r1").style.background = "red";
+    }   
+}
+            
+    document.getElementById("a1").addEventListener("click", valider);
+    document.getElementById("r1").addEventListener("click", valider);
+        
+
+function valider2 () {    
+
+    if (this.id == "a2") {
+        document.getElementById("a2").style.background = "green"; 
+        document.getElementById("r2").style.background = "grey"; 
+    }
+
+    else {
+        document.getElementById("a2").style.background = "grey"; 
+        document.getElementById("r2").style.background = "red";
+    }   
+}
+            
+    document.getElementById("a2").addEventListener("click", valider2);
+    document.getElementById("r2").addEventListener("click", valider2);
+
+function valider3 () {    
+
+    if (this.id == "a3") {
+        document.getElementById("a3").style.background = "green"; 
+        document.getElementById("r3").style.background = "grey"; 
+    }
+
+    else {
+        document.getElementById("a3").style.background = "grey"; 
+        document.getElementById("r3").style.background = "red";
+    }   
+}
+            
+    document.getElementById("a3").addEventListener("click", valider3);
+    document.getElementById("r3").addEventListener("click", valider3);
+
+
+function close () {
+    {
+    console.log();               
+    document.getElementById("popin").style.display = "none";
+    }
+}
+
+document.getElementById("popin2").addEventListener("click", close);
+
 
 function bonjour() {    
     let saisie = document.getElementById("prenom").value;
@@ -43,10 +124,11 @@ function merci() {
     }
 }
     document.getElementById("confirmMail").addEventListener("click", merci);
-
+       
+    
 
         
-
+    
     
 
  
